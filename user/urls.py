e0e2f72fec948da0bbd0c5aebf8408a35bd130de
Jitty Tresa Thomas <jittytresa@gmail.com>
2024-02-13 12:34:50 +0530

    added urls

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+#emmall/user/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
